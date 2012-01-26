983429da05ced115e91b499bb1e49796a62083b4
ibermejo <ibermejo@localhost>
2012-01-26 12:46:50 +0100

    Changed VarEliminationBN's annotation name to VariableElimination

--- src/test/java/inference/InferenceManagerTest.java
+++ src/test/java/inference/InferenceManagerTest.java
@@ -1,5 +1,6 @@
 package inference;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -8,11 +9,13 @@ import org.junit.Test;
 import org.openmarkov.core.inference.InferenceAlgorithm;
 import org.openmarkov.core.inference.annotation.InferenceManager;
 import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.inference.variableElimination.VarEliminationBN;
 
 public class InferenceManagerTest
 {
     private InferenceManager inferenceManager;
     private ProbNet probNet;
+    private final static String VariableEliminationName  = "VariableElimination";
     
     @Before
     public void setUp() throws Exception {
@@ -23,13 +26,21 @@ public class InferenceManagerTest
     @Test
     public void testGetInferenceAlgorithms() throws Exception{
         
-        assertTrue( inferenceManager.getInferenceAlgorithms (probNet).contains ("VariableElimination for BN"));
+        assertTrue( inferenceManager.getInferenceAlgorithms (probNet).contains (VariableEliminationName));
     }
     
     @Test
     public void testGetInferenceAlgorithmsByName() throws Exception{
         
-        InferenceAlgorithm algorithm = inferenceManager.getInferenceAlgorithmByName ("VariableElimination for BN", probNet);
+        InferenceAlgorithm algorithm = inferenceManager.getInferenceAlgorithmByName (VariableEliminationName, probNet);
         assertNotNull(algorithm);
     }    
+    
+    @Test
+    public void testGetDefaultInferenceAlgorithm() throws Exception{
+        
+        InferenceAlgorithm algorithm = inferenceManager.getDefaultInferenceAlgorithm (probNet);
+        assertNotNull(algorithm);
+        assertEquals(algorithm.getClass (), VarEliminationBN.class);
+    }        
 }
