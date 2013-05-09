2982f9d13f9890ff058bea1fd5d3bb5cea317881
ibermejo <ibermejo@localhost>
2013-05-09 10:28:56 +0100

    Removed unused variable in test

--- src/test/java/org/openmarkov/core/gui/io/format/annotation/NetsIOTest.java
+++ src/test/java/org/openmarkov/core/gui/io/format/annotation/NetsIOTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.net.URL;
 
 import org.junit.Test;
 import org.openmarkov.core.gui.dialog.io.NetsIO;
@@ -34,7 +33,6 @@ public class NetsIOTest {
 		String fileNameOpen = null;
 		String fileNameSave = null;
 		String path = null;
-		URL url = this.getClass().getClassLoader().getResource(fileToOpen);
 		//URI uri = getClass().getResource(fileToOpen).toURI();
 		file = new File(getClass().getClassLoader().getResource(fileToOpen).toURI());
 		fileNameOpen = file.getAbsolutePath();
