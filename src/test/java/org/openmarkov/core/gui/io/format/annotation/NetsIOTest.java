8d3d0b4a81e2391a083417bd27f58ffbdecfbfc2
jorgepmartin <jorgepmartin@localhost>
2014-12-16 09:11:58 +0100

    Added IO tests for the nets in the bitbucket's repository

--- src/test/java/org/openmarkov/core/gui/io/format/annotation/NetsIOTest.java
+++ src/test/java/org/openmarkov/core/gui/io/format/annotation/NetsIOTest.java
@@ -4,10 +4,19 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.List;
 
 import org.junit.Test;
+import org.openmarkov.core.exception.ParserException;
+import org.openmarkov.core.exception.WriterException;
 import org.openmarkov.core.gui.dialog.io.NetsIO;
 import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.io.probmodel.PGMXReader;
+import org.openmarkov.io.probmodel.PGMXWriter;
+import org.openmarkov.io.probmodel.bitbucket.NetsRepository;
 
 
 
@@ -65,5 +74,42 @@ public class NetsIOTest {
 		openSaveNetwork("Net1.elv", "Net1Saved.elv");
 		openSaveNetwork("Net2.elv", "Net2Saved.elv");
 		
+	}
+	
+	@Test
+	public final void testOpenSaveRepositoryNets(){
+		NetsRepository repository = new NetsRepository();
+        List<URL> listURL = repository.getNetworks();
+
+        for (URL url : listURL) {
+        	// The name is irrelevant because this nets will only be created for tests purposes and it will be deleted
+        	// after each iteration
+            String networkName = url.getPath();
+            networkName = networkName.substring(networkName.lastIndexOf("/") + 1, networkName.length());
+
+            PGMXReader pgmxReader = new PGMXReader();
+            
+			try {
+				ProbNet probNet = pgmxReader.loadProbNet(url, networkName).getProbNet();
+				assertNotNull(probNet);
+				assertNotNull(probNet.getNodes());
+				
+				PGMXWriter pgmxWritter = new PGMXWriter();
+				pgmxWritter.writeProbNet(networkName, probNet);
+				
+				FileInputStream file = new FileInputStream(networkName);
+				probNet = pgmxReader.loadProbNet(file, networkName).getProbNet();
+				assertNotNull(probNet);
+				assertNotNull(probNet.getNodes());
+
+				
+			} catch (WriterException | FileNotFoundException | ParserException e) {
+				e.printStackTrace();
+				
+			} finally{
+				File fileToBeDeleted = new File(networkName);
+				fileToBeDeleted.delete();
+			}
+        }
 	}
 }
