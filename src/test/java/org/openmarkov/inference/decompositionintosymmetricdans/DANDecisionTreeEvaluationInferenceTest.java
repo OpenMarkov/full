b291476894fb64b22bd0dd4504f5fe8489dc5fd2
mluque <mluque@localhost>
2017-11-10 20:13:58 +0100

    Implementing DAN evaluation algorithms.

--- src/test/java/org/openmarkov/inference/decompositionintosymmetricdans/DANDecisionTreeEvaluationInferenceTest.java
+++ src/test/java/org/openmarkov/inference/decompositionintosymmetricdans/DANDecisionTreeEvaluationInferenceTest.java
@@ -1,10 +1,6 @@
 package org.openmarkov.inference.decompositionintosymmetricdans;
 
-import org.junit.Test;
-import org.openmarkov.core.exception.IncompatibleEvidenceException;
-import org.openmarkov.core.exception.NodeNotFoundException;
 import org.openmarkov.core.exception.NotEvaluableNetworkException;
-import org.openmarkov.core.exception.UnexpectedInferenceException;
 import org.openmarkov.core.model.network.ProbNet;
 import org.openmarkov.inference.decompositionIntoSymmetricDANs.DANDecisionTreeEvaluation;
 import org.openmarkov.inference.decompositionIntoSymmetricDANs.DANEvaluation;
@@ -20,26 +16,5 @@ public class DANDecisionTreeEvaluationInferenceTest extends DANEvaluationInferen
 			e.printStackTrace();
 		}
 		return eval;
-	}
-	
-	//TODO This test belongs to DANEvaluationInferenceTest. The corresponding must be solved, and then, this method must be removed
-	@Test
-	public void testDANTest2TherapiesNoCostSymmetrizedOrderForced() throws IncompatibleEvidenceException, UnexpectedInferenceException, NodeNotFoundException, NotEvaluableNetworkException{
-	//	testMEUAndIntervention("decide-test-2therapies-no-cost-symmetrized-order-forced",9.39366,"Do test?","Result of test","Therapy");
-	}
-
-	//TODO This test belongs to DANEvaluationInferenceTest. The corresponding must be solved, and then, this method must be removed
-		//@Test
-	@Test
-	public void testDANTest2TherapiesNoCostOrderForced() throws IncompatibleEvidenceException, UnexpectedInferenceException, NodeNotFoundException, NotEvaluableNetworkException{
-		//testMEUAndIntervention("decide-test-2therapies-no-cost-order-forced",9.39366,"Do test?","Result of test","Therapy");
-	}
-
-	//TODO This test belongs to DANEvaluationInferenceTest. The corresponding must be solved, and then, this method must be removed
-		//@Test
-	@Test
-	public void testDANTest2TherapiesNoCost() throws IncompatibleEvidenceException, UnexpectedInferenceException, NodeNotFoundException, NotEvaluableNetworkException{
-		//testMEUAndIntervention("decide-test-2therapies-no-cost",9.39366,"Do test?","Result of test","Therapy");
-	}
-
+	}	
 }
