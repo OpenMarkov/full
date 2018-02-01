5b83e758ebb1998c441b014321f789c3c005c40d
Jorge Pérez <jperezmartin@dia.uned.es>
2018-02-01 10:47:06 +0100

    Merge remote-tracking branch 'origin/development' into development
    # Conflicts:
    #	src/main/java/VETemporalEvaluation.java
    #	src/main/java/org/openmarkov/inference/temporalevaluation/TemporalEvaluation.java
    #	src/main/java/org/openmarkov/inference/temporalevaluation/tasks/TemporalEvaluation.java

--- src/test/java/org/openmarkov/integrationTests/midCochlearTests.java
+++ src/test/java/org/openmarkov/integrationTests/midCochlearTests.java
@@ -14,7 +14,7 @@ import org.openmarkov.core.io.ProbNetInfo;
 import org.openmarkov.core.model.network.*;
 import org.openmarkov.core.model.network.potential.GTablePotential;
 import org.openmarkov.core.model.network.potential.TablePotential;
-import org.openmarkov.inference.temporalevaluation.TemporalEvaluation;
+import org.openmarkov.inference.temporalevaluation.tasks.TemporalEvaluation;
 import org.openmarkov.inference.variableElimination.tasks.VECEAnalysis;
 import org.openmarkov.io.probmodel.reader.PGMXReader;
 
