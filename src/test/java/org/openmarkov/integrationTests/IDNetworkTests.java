fa8dff3be93faa649366b81884d8e59603549713
Manuel Luque <mluque@dia.uned.es>
2021-06-25 16:36:29 +0200

    Adding more networks for tests, including uncertain values in TreeADDs.

---
+++ src/test/java/org/openmarkov/integrationTests/IDNetworkTests.java
@@ -0,0 +1,72 @@
+package org.openmarkov.integrationTests;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmarkov.core.exception.IncompatibleEvidenceException;
+import org.openmarkov.core.exception.NotEvaluableNetworkException;
+import org.openmarkov.core.exception.ParserException;
+import org.openmarkov.core.io.ProbNetInfo;
+import org.openmarkov.core.io.ProbNetReader;
+import org.openmarkov.core.model.network.EvidenceCase;
+import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.core.model.network.modelUncertainty.AxisVariation;
+import org.openmarkov.core.model.network.modelUncertainty.DeterministicAxisVariationType;
+import org.openmarkov.core.model.network.modelUncertainty.SystematicSampling;
+import org.openmarkov.core.model.network.modelUncertainty.UncertainParameter;
+import org.openmarkov.core.model.network.potential.TablePotential;
+import org.openmarkov.inference.variableElimination.tasks.VESensAnTornadoSpider;
+import org.openmarkov.io.probmodel.reader.PGMXReader_0_2;
+
+public abstract class IDNetworkTests {
+	
+	protected String networkName;
+
+	// Delta parameter for Assert.Equals methods
+	protected final double deltaEquals = Math.pow(10, -4);
+
+	protected ProbNet probNet;
+	protected EvidenceCase preResolutionEvidence;
+
+	@Before public void setUp() throws Exception {
+		InputStream file = getClass().getClassLoader().getResourceAsStream(networkName);
+
+		// Load the network: ID-decide-test
+		ProbNetReader pgmxReader = newPGMXReader();
+		ProbNetInfo probNetInfo = null;
+		try {
+			probNetInfo = pgmxReader.loadProbNetInfo(networkName, file);
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+		this.probNet = probNetInfo.getProbNet();
+		if (probNetInfo.getEvidence().size() != 0) {
+			this.preResolutionEvidence = probNetInfo.getEvidence().get(0);
+		}
+	}
+	
+	protected ProbNetReader newPGMXReader() {
+		return new PGMXReader_0_2();
+	}
+
+	@Test public void veSensAnTornadoSpiderTests() {
+		List<UncertainParameter> uncertainParameterList = SystematicSampling.getUncertainParameters(this.probNet);
+		AxisVariation axisVariation = new AxisVariation();
+		axisVariation.setVariationType(DeterministicAxisVariationType.POPP);
+		axisVariation.setVariationValue(0.8);
+
+		try {
+			VESensAnTornadoSpider veSensAnTornadoSpider = new VESensAnTornadoSpider(probNet, preResolutionEvidence,
+					uncertainParameterList, axisVariation, 50);
+			HashMap<UncertainParameter, TablePotential> uncertainParameterTablePotentialHashMap = veSensAnTornadoSpider
+					.getUncertainParametersPotentials();
+
+		} catch (NotEvaluableNetworkException | IncompatibleEvidenceException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
