cb19eba888c5a0938fe2cdcf3ecea41b5dc6f5ec
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2026-05-06 17:23:13 +0200

    [Refactor] Moved all of org.openmarkov.io.database.* modules into org.openmarkov.io. This simplifies the solution to issue 608.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ open module org.openmarkov.full {
 	requires org.openmarkov.gui;
 	requires org.openmarkov.inference;
 	requires org.openmarkov.io;
-	requires org.openmarkov.io.database.elvira;
 	requires org.apache.logging.log4j;
 	requires org.apache.logging.log4j.core;
 	requires org.apache.poi.poi;
